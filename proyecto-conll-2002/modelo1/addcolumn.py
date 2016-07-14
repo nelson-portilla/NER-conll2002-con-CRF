c8104645d69d6eab477f542d12e9dc585da91514
brayan-rodriguez <brayan.rodriguez@alertlogic.com>
2016-07-14 11:12:17 -0500

    para probar 2 feature

--- proyecto-conll-2002/modelo1/addcolumn.py
+++ proyecto-conll-2002/modelo1/addcolumn.py
@@ -1,5 +1,10 @@
 import sys, os
 
+def getTipo(palabra):
+	listaAM=str(os.popen('echo '+palabra+'| analyze -f am.cfg').read()[:-1]).split()
+	eagles=listaAM[1:]
+	return eagles[1][0]
+
 #Primera Caracteristica: Uno si la primera letra es Mayuscula, 0 lo contrario
 def addFirstFeature(pathin, pathout):
 	diccionario={'A':[1,3,4,0], 'R':[1,0,0,0]}
@@ -7,10 +12,12 @@ def addFirstFeature(pathin, pathout):
 	salida=open(pathout, 'w')
 	binario={'True':"1", 'False':"0"}
 	for linea in filas:
-		if (linea[0]!='\n'):					
-			flag=linea[0].isupper()
-			lista=linea.split()
-			lista.insert(2,binario[str(flag)])		
+		if (linea[0]!='\n'):
+			lista=linea.split()		
+			firstfeature=linea[0].isupper()
+			word = lista[0]
+			secondfeature = getTipo(word)
+			lista.insert(2,binario[str(firstfeature)])		
 			for palabra in lista:
 				salida.write(str(palabra+" "))
 			salida.write("\n")
@@ -22,7 +29,7 @@ def addFirstFeature(pathin, pathout):
 
 
 if __name__ == '__main__':
-	print "Iniciando..."
+	#print "Iniciando..."
 	addFirstFeature("esp.train","train")
 	# addFirstCaracter("mytrain","modelo1/train")
 	addFirstFeature("esp.testb","test")
