b65b538f21948bf48395277a3d4313bc5f9db3b8
UV <brayan.rodriguez.rivera@correounivalle.edu.co>
2016-07-16 19:07:46 -0500

    todas las caracteristicas, SIN PROBAR

--- proyecto-conll-2002/modelo1/addcolumn.py
+++ proyecto-conll-2002/modelo1/addcolumn.py
@@ -1,9 +1,24 @@
 import sys, os
+#tipo, genero, numero, persona
+dic= {
+	'A': [1, 3, 4],
+	'R':[1],
+	'D':[1,3,4,2],
+	'N': [1,2,3],
+	'V': [1,6,5,4],
+	'P': [1,3,4,2],
+	'C':[1],
+	'I':[],
+	'S':[1,2,3],
+	'F':[],
+	'Z':[1],
+	'W':[],
+}
 
-def getTipo(palabra):
+def getEagles(palabra):
 	listaAM=str(os.popen('echo '+palabra+'| analyze -f am.cfg').read()[:-1]).split()
 	eagles=listaAM[1:]
-	return eagles[1][0]
+	return eagles
 
 #Primera Caracteristica: Uno si la primera letra es Mayuscula, 0 lo contrario
 def addFirstFeature(pathin, pathout):
@@ -16,8 +31,39 @@ def addFirstFeature(pathin, pathout):
 			lista=linea.split()		
 			firstfeature=linea[0].isupper()
 			word = lista[0]
-			secondfeature = getTipo(word)
-			lista.insert(2,binario[str(firstfeature)])		
+			post = lista[1]
+			eagles = getEagles(word)
+			print eagles
+			ls= dic[post[0]]
+			index = [int(i[0]) for i in eagles].index(post[0])
+			eagles_sing = eagles[index]
+			print eagles_sing
+			try:
+				tipo= eagles_sing[ls[0]]
+				print tipo
+			except IndexError:
+				tipo = 0
+				print "no tipo"
+			try:
+				genero= eagles_sing[ls[1]]
+				print genero
+			except IndexError:
+				genero=0
+				print "no genero"
+			try:
+				numero= eagles_sing[ls[2]]
+				print numero
+			except IndexError:
+				numero=0
+				print "no numero"
+			try:
+				persona= eagles_sing[ls[3]]
+				print persona
+			except IndexError:
+				persona=0
+				print "no persona"
+			lista.insert(2,binario[str(firstfeature)])
+			lista.insert(3, tipo)
 			for palabra in lista:
 				salida.write(str(palabra+" "))
 			salida.write("\n")
